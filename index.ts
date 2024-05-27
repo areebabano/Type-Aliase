ea5fa762a69f1f5920bc4e4a4ad42971ac5ccda0
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-05-28 00:38:56 +0500

    type aliase in Typescript

---
+++ index.ts
@@ -0,0 +1,37 @@
+// Type Alias 
+
+type stuData = {
+    name: string,
+    institute: string
+    id: number
+}
+
+
+// object 
+
+let student: stuData = {
+    name: "Areeba",
+    institute: "GIAIC",
+    id: 234
+}
+
+
+let student2: stuData= {
+    name: "Nigar",
+    institute: "GIAIC",
+    id: 123
+}
+
+
+
+// // let student3: {
+//     name: string,
+//     institute: string,
+//     id: number
+// } = {
+//     name: "Nigar",
+//     institute: "GIAIC",
+//     id: 123
+// }
+
+
